bdf5f4f858ea24990eb4f0cf05299d9332c191d3
Jan Hernas <hernasj@wp.pl>
2025-05-06 18:43:01 +0200

    Add algorithm launches for testing

---
+++ leo_simulator-ros2/leo_gz_bringup/launch/cartographer.launch.py
@@ -0,0 +1,67 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    resolution = LaunchConfiguration('resolution', default='0.05')
+    publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
+
+    carographer_prefix = get_package_share_directory('leo_gz_bringup')
+    cartographer_config_dir = LaunchConfiguration(
+        'cartographer_config_dir',
+        default=os.path.join(carographer_prefix, 'config')
+    )
+    configuration_basename = LaunchConfiguration('configuration_basename',
+                                                 default='cartographer_config.lua')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'cartographer_config_dir',
+            default_value=cartographer_config_dir,
+            description='Full path to config file to load'),
+        DeclareLaunchArgument(
+            'configuration_basename',
+            default_value=configuration_basename,
+            description='Name of lua file for cartographer'),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+        DeclareLaunchArgument(
+            'resolution',
+            default_value=resolution,
+            description='Resolution of a grid cell in the published occupancy grid'),
+        DeclareLaunchArgument(
+            'publish_period_sec',
+            default_value=publish_period_sec,
+            description='OccupancyGrid publishing period'),
+
+        # Cartographer Node
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_node',
+            name='cartographer_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-configuration_directory', cartographer_config_dir,
+                       '-configuration_basename', configuration_basename],
+            remappings=[
+                ('/scan', '/lidar/laserscan')
+            ]
+        ),
+
+        # Occupancy Grid Node
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_occupancy_grid_node',
+            name='cartographer_occupancy_grid_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-resolution', resolution, '-publish_period_sec', publish_period_sec]
+        ),
+    ])
